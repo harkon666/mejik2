dbef1ef48e7d033fca2c400f4df753aeb89e0353
harkon666 <bryan.wicaksanaa@gmail.com>
2020-05-09 23:17:46 +0700

    creating screen article for testing

---
+++ mejik/src/screens/BottomTabNavigator/article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ArticleScreen = () => {
+  return (
+    <View>
+      <Text>Hallo ini screen article</Text>
+    </View>
+  );
+};
+
+export default ArticleScreen;
--- mejik/src/screens/BottomTabNavigator/index.js
+++ mejik/src/screens/BottomTabNavigator/index.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+//screen
+import ArticleScreen from './article';
+
+const Router = createBottomTabNavigator({
+  ArticleScreen: ArticleScreen,
+});
